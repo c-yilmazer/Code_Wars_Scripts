14518875b80381397834c1e5e0be6b795dfdc21d
Cagatay Yilmazer <36925088+c-yilmazer@users.noreply.github.com>
2025-09-08 19:00:02 -0400

    Add files via upload

---
+++ 6kyu_replace_with_alph_position.py
@@ -0,0 +1,24 @@
+#Welcome.
+
+#In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+#If anything in the text isn't a letter, ignore it and don't return it.
+
+#"a" = 1, "b" = 2, etc.
+
+#Example
+#Input = "The sunset sets at twelve o' clock."
+#Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+
+import string
+
+def alphabet_position(text):
+    new_text = " "
+    for char in text:
+        if char.isalpha():
+            new_text += str(string.ascii_lowercase.index(char.lower())+1) + " "
+        else: 
+            continue
+    return new_text.strip()
+
+print(alphabet_position("The sunset sets at twelve o' clock."))
